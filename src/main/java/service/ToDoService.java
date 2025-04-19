beed53b3fa4a64d965f3826bc219a6b67ddd06ae
Tinton Despi Alkhifari <tintondespi@gmail.com>
2025-04-19 14:50:40 +0700

    INIT GITHUB

---
+++ src/main/java/service/ToDoService.java
@@ -0,0 +1,32 @@
+package service;
+
+import model.ToDo;
+import repository.ToDoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ToDoService {
+
+    @Autowired
+    private ToDoRepository toDoRepository;
+
+    public List<ToDo> getAllTodos() {
+        return toDoRepository.findAll();
+    }
+
+    public void saveTodo(ToDo todo) {
+        toDoRepository.save(todo);
+    }
+
+    public void deleteTodo(Long id) {
+        toDoRepository.deleteById(id);
+    }
+
+    public Optional<ToDo> getTodoById(Long id) {
+        return toDoRepository.findById(id);
+    }
+}
